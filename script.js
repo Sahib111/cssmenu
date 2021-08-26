af5c84974934cc5b523ad2f068125bdf473cf71e
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2021-08-26 17:00:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+//<![CDATA[
+    function toggleCssMenu(icon) {
+        var cssmenu = document.getElementById('cssmenu');
+        if (icon.className.indexOf('active') == -1) {
+            icon.className = 'menu-icon active';
+			cssmenu.style.display = "block";
+			setTimeout(function(){cssmenu.className = 'active';},0);            
+        }
+        else {
+            icon.className = 'menu-icon';
+			cssmenu.className = '';
+			setTimeout(function(){cssmenu.style.display = "none";},411); 
+        }
+    }
+//]]>
